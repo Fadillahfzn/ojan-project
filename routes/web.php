942780de520e921fd07bcf03bd9b0ad9d85bdbd9
fadillahfzn <72147683+Fadillahfzn@users.noreply.github.com>
2025-01-08 10:57:28 +0700

    Update create product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +24,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/product/create', [ProductController::class, 'create_product'])->name('create_product');
+Route::post('/product/create', [ProductController::class, 'store_product'])->name('store_product');
